6a98cff6ff2180dfb8e45a06c960a0c5c3dd1023
Putu Harum Bawa <lightharum.108@gmail.com>
2021-10-12 08:11:07 +0800

    add vue and react

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,8 @@
-export default {
-  root: './src/apps/web-client'
-}
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  root: './src/apps/web-client',
+  plugins: [vue(), react()],
+});
