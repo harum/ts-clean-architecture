96d34d21bacbf092982d8221453a96562b63f17e
Putu Harum Bawa <lightharum.108@gmail.com>
2021-10-15 13:22:25 +0800

    update repo

--- src/lib/toggle/repositories/ToggleFeatureLocalStorageRepository.ts
+++ src/lib/toggle/repositories/ToggleFeatureLocalStorageRepository.ts
@@ -2,27 +2,36 @@ import ToggleFeature from '../entities/ToggleFeature';
 import TogglePlatform from '../entities/TogglePlatform';
 import IToggleFeatureRepository from './IToggleFeatureRepository';
 
+const localStorageKey = 'toggleFeatures';
+
 export default class ToggleFeatureLocalStorageRepository
   implements IToggleFeatureRepository
 {
   async findAll(limit = 10): Promise<Array<ToggleFeature>> {
-    const rowInfo = JSON.parse(localStorage.getItem('df') || '');
-    const toggleFeature = new ToggleFeature();
-    toggleFeature.buildFromHash(rowInfo);
+    const rows = JSON.parse(localStorage.getItem(localStorageKey) || '[]');
 
-    return [toggleFeature];
+    return rows.map((row: any) => {
+      const toggleFeature = new ToggleFeature();
+      toggleFeature.buildFromHash(row);
+
+      return toggleFeature;
+    });
   }
 
   async find(feature: string): Promise<ToggleFeature> {
-    const rowInfo = JSON.parse(localStorage.getItem(feature) || '');
+    const rows = JSON.parse(localStorage.getItem(localStorageKey) || '[]');
+    const row = rows.find((toggle: any) => toggle.feature === feature);
     const toggleFeature = new ToggleFeature();
-    toggleFeature.buildFromHash(rowInfo);
+    toggleFeature.buildFromHash(row);
 
     return toggleFeature;
   }
 
   async save(toggleFeature: ToggleFeature): Promise<ToggleFeature> {
-    localStorage.setItem(toggleFeature.feature, toggleFeature.toString());
+    const rows = JSON.parse(localStorage.getItem(localStorageKey) || '[]');
+    rows.push(toggleFeature.toHash());
+
+    localStorage.setItem(localStorageKey, JSON.stringify(rows));
 
     return toggleFeature;
   }
